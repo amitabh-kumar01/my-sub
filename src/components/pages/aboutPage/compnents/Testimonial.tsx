74db4ffb172ed7d4e90b6b06dca33cc11e9c395b
Amitabh kumar <amitabhkumar01.hp@outlook.com>
2024-09-15 17:47:59 +0530

    jfdlkdsjfkldsafdkjsfkajsdflas

--- src/components/pages/aboutPage/compnents/Testimonial.tsx
+++ src/components/pages/aboutPage/compnents/Testimonial.tsx
@@ -1,26 +1,38 @@
+"use client"
 import React from "react";
-import ReviewCard from "../../landingpage/components/reviewCard";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 import { GoArrowLeft, GoArrowRight } from "react-icons/go";
+import NewReviewCard from "./newReviewCard";
 
 const Testimonial = () => {
+  const reviewCardRef = React.useRef<{ scrollLeft: () => void; scrollRight: () => void }>(null);
+
   return (
     <div className="bg-gray-100 pb-10">
       <div className="flex md:justify-between md:px-20 justify-around ">
         <h3 className="text-xl md:text-2xl lg:text-3xl text-justify font-bold text-customDarkBlue mb-4 ">
-          Client Testimonail
+          Client Testimonial
         </h3>
         <div className="flex gap-2 text-black">
-          <span className="text-center p-1 font-light  text-xl hover:text-white hover:bg-green-500 w-7 h-7 flex items-center border rounded-full">
-            <GoArrowLeft/>
+          {/* Left Arrow */}
+          <span
+            className="text-center p-1 font-light  text-xl hover:text-white hover:bg-green-500 w-7 h-7 flex items-center border rounded-full"
+            onClick={() => reviewCardRef.current?.scrollLeft()}
+          >
+            <GoArrowLeft />
           </span>
-          <span className="text-center p-1 text-xl hover:bg-green-500 w-7 h-7 flex items-center border font-thin hover:text-white   rounded-full">
-            <GoArrowRight/>
+          {/* Right Arrow */}
+          <span
+            className="text-center p-1 text-xl hover:bg-green-500 w-7 h-7 flex items-center border font-thin hover:text-white   rounded-full"
+            onClick={() => reviewCardRef.current?.scrollRight()}
+          >
+            <GoArrowRight />
           </span>
         </div>
       </div>
       <div className="md:pl-10">
-        <ReviewCard />
+        {/* Pass ref to ReviewCard */}
+        <NewReviewCard ref={reviewCardRef} />
       </div>
     </div>
   );
